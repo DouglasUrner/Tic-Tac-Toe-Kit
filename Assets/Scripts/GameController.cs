f140945dfa0f02a7f0cd8ffb999b47c530728942
Douglas Urner <dlu@canishe.com>
2019-09-06 19:23:59 -0700

    Initial commit of Unity code.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text[] spaceList;
+    public GameObject gameOverPanel;
+    public Text gameOverText;
+    public GameObject restartButton;
+
+    private string side;
+    private int moves;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetGameControllerReferenceForButtons();
+  
+        gameOverPanel.SetActive(false);
+        restartButton.SetActive(false);
+
+        side = "X";
+        moves = 0;
+    }
+
+    void SetGameControllerReferenceForButtons()
+    {
+        for (int i = 0; i < spaceList.Length; i++)
+        {
+            spaceList[i].GetComponentInParent<Space>().SetControllerReference(this);
+        }
+    }
+}
